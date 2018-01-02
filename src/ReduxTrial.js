e29cf1f870453251d6cbb0e34b14843d0f2f81eb
kazuma1989 <funifuni.1204@gmail.com>
2018-01-02 02:28:09 +0000

    Added simple Redux sample

--- src/ReduxTrial.js
+++ src/ReduxTrial.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStore } from 'redux';
 
 // https://redux.js.org/#the-gist
@@ -28,58 +29,50 @@ import { createStore } from 'redux';
   // 11
 }
 
+// Reducer
+function formReducer(state, action) {
+  switch (action.type) {
+    case 'SEND':
+      return Object.assign({}, state, {
+        value: action.value,
+      });
+    default:
+      return state;
+  }
+}
 
-// /* Storeの実装 */
+// createStore（）メソッドを使ってStoreの作成
+const initialState = {
+  value: null,
+};
+const store = createStore(formReducer, initialState);
 
-// // 初期state変数（initialState）の作成
-// const initialState = {
-//   value: null,
-// };
-// // createStore（）メソッドを使ってStoreの作成
-// const store = createStore(formReducer, initialState);
+// stateの状態を購読。状態に変化があったらリスナーを実行
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(state);
+});
 
-// // ActionをReducerに伝播
-// store.dispatch(actionCreators());
+// ActionをReducerに伝播
+store.dispatch(send('first'));
 
-
-// // stateの状態を購読。状態に変化があったらリスナーを実行
-// store.subscribe(() => {
-
-//   /* リスナーの処理を書く */
-
-//   // stateを取得
-//   const state = store.getState();
-//   console.log(state);
-// });
-
-// /* Actionの実装 */
-
-// // Action名の定義
-// const SEND = 'SEND';
-
-// // Action Creator
-// function send(value) {
-//   // Action
-//   return {
-//     type: SEND,
-//     value,
-//   };
-// }
-
-// // Reducer
-// function formReducer(state, action) {
-//   switch (action.type) {
-//     case SEND:
-//       return Object.assign({}, state, {
-//         value: action.value,
-//       });
-//     default:
-//       return state;
-//   }
-// }
+// Action Creator
+function send(value) {
+  // Action
+  return {
+    type: 'SEND',
+    value,
+  };
+}
 
 // function actionCreators() {
-//   return send('foo');
+//   return send('first');
 // }
 
-export const reduxApp = null;
+function FormApp(props) {
+  return (
+    <div></div>
+  );
+}
+
+export const reduxApp = <FormApp />;
